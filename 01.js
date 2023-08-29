387ecd2af06d003a52b5911dae542935d7d19d77
hansg0325 <hansg0325@gmail.com>
2023-08-30 00:25:35 +0900

    전체적인 스타일링 수정 및 삭제 버튼 추가

--- 01.js
+++ 01.js
@@ -1,12 +1,38 @@
-const $ul = document.querySelector('ul');
-const $input = document.querySelector('#input');
+document.addEventListener('DOMContentLoaded', () => {
+  const toDo = document.querySelector('#toDo')
+  const addButton = document.querySelector('#addButton')
+  const toDoList = document.querySelector('#toDoList')
 
-$input.addEventListener('keyup', (e) => {
-  if (e.key !== 'Enter') return;
+  addButton.addEventListener('click', (event) => {
+    const item = document.createElement('div') //영역 생성
 
-  $newLi = document.createElement('li');
-  $newLi.textContent = e.target.value;
-  $ul.appendChild($newLi);
+    const checkBox = document.createElement('input') //체크박스생성
+    checkBox.setAttribute('type', 'checkbox')
 
-  e.target.value = '';
-});
+    const text = document.createElement('span') //span생성 후 toDo.value로 초기화
+    text.textContent = toDo.value
+
+    const removeButton = document.createElement('button') //삭제버튼 생성
+    removeButton.textContent = '삭제'
+
+    //삭제버튼 클릭 시  삭제
+    removeButton.addEventListener('click', (event) => {
+      event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode)
+    })
+
+    checkBox.addEventListener('change', (event) => {
+      if(checkBox.checked){
+        text.style.textDecorationLine = "line-through"
+      }else{
+        text.style.textDecorationLine = "none"
+      }
+    })
+
+    item.appendChild(checkBox)
+    item.appendChild(text)
+    item.appendChild(removeButton)
+
+    toDoList.appendChild(item)
+    toDo.value = ''
+  })
+})
